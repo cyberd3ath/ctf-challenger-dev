ad88e7cc1a0bb06179a93f1e55701e0111045700
KunoVonHagen <41991675+KunoVonHagen@users.noreply.github.com>
2025-08-10 02:24:41 +0200

    trying something else again

--- testing/utils/mock_db.py
+++ testing/utils/mock_db.py
@@ -8,16 +8,31 @@ load_dotenv()
 TESTING_DATABASE_BASE_DIR = os.getenv("TESTING_DATABASE_BASE_DIR", "/tmp/pg_test_base")
 
 
+POSTGRES_BINARIES_PATHS = None
+for path in [
+    "/usr/lib/postgresql/16/bin",
+    "/usr/lib/postgresql/15/bin",
+    "/usr/lib/postgresql/14/bin",
+    "/usr/lib/postgresql/13/bin"
+]:
+    if os.path.exists(path):
+        POSTGRES_BINARIES_PATHS = path
+        break
+
+INITDB_PATH = os.path.join(POSTGRES_BINARIES_PATHS, "initdb") if POSTGRES_BINARIES_PATHS else None
+POSTGRES_PATH = os.path.join(POSTGRES_BINARIES_PATHS, "postgres") if POSTGRES_BINARIES_PATHS else None
+
 
 class MockDatabase:
     def __init__(self):
         self.postgresql = None
         self.conn = None
 
-        original_getuid = os.getuid
-        os.getuid = lambda: 1000  # fake non-root UID
-        self.postgresql = testing.postgresql.Postgresql(base_dir=TESTING_DATABASE_BASE_DIR)
-        os.getuid = original_getuid
+        self.postgresql = testing.postgresql.Postgresql(
+            copy_data_from=TESTING_DATABASE_BASE_DIR,
+            initdb=INITDB_PATH,
+            postgres=POSTGRES_PATH,
+        )
 
         dsn = self.postgresql.dsn()
         self.conn = psycopg2.connect(**dsn)
