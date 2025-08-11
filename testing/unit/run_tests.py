51d138337ffe503883a8fd45f6c01f4154ab666e
KunoVonHagen <41991675+KunoVonHagen@users.noreply.github.com>
2025-08-11 19:56:07 +0200

    Adding sleep between tests

--- testing/unit/run_tests.py
+++ testing/unit/run_tests.py
@@ -1,3 +1,5 @@
+import time
+
 from test_backend_challenge_handling import test_backend_challenge_handling
 from test_backend_machine_template_handling import test_backend_machine_template_handling
 from test_backend_user_config_handling import test_backend_user_config_handling
@@ -12,8 +14,12 @@ def run_all_tests():
     print("\nTesting the creation and deletion of user configurations")
     test_backend_user_config_handling()
 
+    time.sleep(10)
+
     print("\nTesting the import and deletion of machine templates")
     test_backend_machine_template_handling()
+
+    time.sleep(10)
 
     print("\nTesting the launch and stop of challenges")
     test_backend_challenge_handling()
