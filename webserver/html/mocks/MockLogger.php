cd2a367b402c541a1c649ec39553c0e09f6baa16
KunoVonHagen <41991675+KunoVonHagen@users.noreply.github.com>
2025-10-07 06:19:04 +0200

    Implemented MockLogger for unit tests part 2

--- webserver/html/mocks/MockLogger.php
+++ webserver/html/mocks/MockLogger.php
@@ -7,22 +7,22 @@ class MockLogger implements ILogger
 {
     public function logInfo($message): void
     {
-        fwrite(STDERR, print_r($message, true));
+        fwrite(STDERR, print_r($message, true) . "\n");
     }
 
     public function logError($message): void
     {
-        fwrite(STDERR, print_r($message, true));
+        fwrite(STDERR, print_r($message, true) . "\n");
     }
 
     public function logDebug($message): void
     {
-        fwrite(STDERR, print_r($message, true));
+        fwrite(STDERR, print_r($message, true) . "\n");
     }
 
     public function logWarning($message): void
     {
-        fwrite(STDERR, print_r($message, true));
+        fwrite(STDERR, print_r($message, true) . "\n");
     }
 
     public function anonymizeIp(string $ip): string
